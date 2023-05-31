05532fec82f51e2a88037fbd23a316233745c607
Demonstr66 <Demonstr66@yandex.ru>
2023-06-01 00:24:19 +0300

    test some changes

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,5 +2,35 @@
   <div class="about">
     <h1>This is an about page</h1>
     <h2>HELLO FROM  ~CI/CD</h2>
+    <button @click="getData">try load</button>
+    <div style="border-radius: 8px; border: 1px solid grey;">
+      {{response}}
+    </div>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "about",
+  data() {
+    return {
+      response: ""
+    }
+  },
+  methods: {
+    async getData() {
+      axios.get('/api/123')
+          .then((response) => {
+            console.log(response)
+            this.response = response.data
+          })
+          .catch((err) => {
+            console.log(err)
+          })
+
+    }
+  }
+}
+</script>
