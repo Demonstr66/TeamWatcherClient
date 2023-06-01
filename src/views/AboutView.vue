12a048da3aaf5cae979d6c7ce09882df0d31d0a1
Demonstr66 <Demonstr66@yandex.ru>
2023-06-01 10:14:15 +0300

    some changes

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <h2>HELLO FROM  ~CI/CD</h2>
+    <h2>NEW COMMIT</h2>
+    <input v-model="req" type="text" />
     <button @click="getData">try load</button>
     <div style="border-radius: 8px; border: 1px solid grey;">
       {{response}}
@@ -16,12 +17,13 @@ export default {
   name: "about",
   data() {
     return {
+      req: "",
       response: ""
     }
   },
   methods: {
     async getData() {
-      axios.get('/api/123')
+      axios.get(req)
           .then((response) => {
             console.log(response)
             this.response = response.data
--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {
         proxy: {
             '/api': {
-                target: 'http://141.8.198.215:3000/',
+                target: 'http://backend:3000/',
                 ws: true,
                 changeOrigin: true
             }
